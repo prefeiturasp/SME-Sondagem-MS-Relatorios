35b0e870b89e86c96504dd8a02bbcd8e4f6b5944
CiceroHenriqueSantos <50219791+CiceroHenriqueSantos@users.noreply.github.com>
2026-03-18 12:02:26 -0300

    Rota de teste

--- src/SME.Sondagem.MS.Relatorios.Infra/Fila/RotasRabbit.cs
+++ src/SME.Sondagem.MS.Relatorios.Infra/Fila/RotasRabbit.cs
@@ -5,7 +5,7 @@ public class RotasRabbit
     public static string RotaLogs => "ApplicationLog";
     public static string Log => "ApplicationLog";
 
-    public const string RelatorioSondagemPorTurma = "sr.sondagem.relatorios.solicitados.sondagem.por.turma-teste";
+    public const string RelatorioSondagemPorTurma = "sr.sondagem.relatorios.solicitados.sondagem.por.turma";
     public const string RotaRelatoriosProntosSgp = "sgp.relatorios.prontos.notificar";
     //public const string RotaRelatorioComErro = "sgp.relatorios.erro.notificar";
 }
